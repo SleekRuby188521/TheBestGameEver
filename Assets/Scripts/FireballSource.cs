7c96ae723047fc6098e588407c0e04b2c41b9fa0
SleekRuby188521 <asleekruby@gmail.com>
2024-03-03 11:01:44 +0500

    11

--- Assets/Scripts/FireballSource.cs
+++ Assets/Scripts/FireballSource.cs
@@ -7,6 +7,12 @@ public class FireballSource : MonoBehaviour
     public Transform targetPoint;
     public Camera cameraLink;
     public float targetInSkyDistance;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     private void Update()
     {
         var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
